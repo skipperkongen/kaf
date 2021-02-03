5198d5eb9a00a063eac13fffe7497e095cf36107
Joakim Blach Andersen <joakim.blach.andersen@maersk.com>
2021-02-03 12:16:18 +0100

    .

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='kaf',
-     version='v0.2.3',
-     download_url='https://github.com/skipperkongen/kaf/archive/v0.2.3.tar.gz',
-     licence='MIT',
-     author="Pimin Konstantin Kefaloukos",
-     author_email="skipperkongen@gmail.com",
-     description="A mini-framework for Kafka apps",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/skipperkongen/kaf",
-     packages=['kaf'],
-     install_requires=[
-        'confluent_kafka',
+    name='kaf',
+    version='v0.2.4',
+    download_url='https://github.com/skipperkongen/kaf/archive/v0.2.4.tar.gz',
+    licence='MIT',
+    author="Pimin Konstantin Kefaloukos",
+    author_email="skipperkongen@gmail.com",
+    description="A mini-framework for Kafka apps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skipperkongen/kaf",
+    packages=['kaf'],
+    install_requires=[
+         'confluent_kafka',
         'retrying'
-     ],
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
